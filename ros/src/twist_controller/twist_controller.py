c99974512afd4d577a403097fb0e3286b12cadfb
Unknown <liuyusun@hotmail.com>
2017-09-21 21:25:38 -0700

    feat: use new torque value

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -14,8 +14,6 @@ class Controller(object):
         self.accel_limit = accel_limit
         self.brake_deadband = brake_deadband
         self.pid = PID(kp,ki,kd,-1,1)
-        self.pid_throttle = PID(kp,ki,kd,-1,1)
-        self.pid_brake = PID(2*kp,ki,kd,-1,1)
 
         self.yaw = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
 
@@ -28,14 +26,7 @@ class Controller(object):
 
         error = target_linear_vel - current_linear_vel
 
-        # val = self.pid.step(error,dt)
-        val = 0
-        if error >= 0:
-            val = self.pid_throttle.step(error,dt)
-        else:
-            val = self.pid_brake.step(error,dt)
-
-        # rospy.logwarn("Target v  %.03f  Error : %.03f  Control:%.03f", target_linear_vel,error,val)
+        val = self.pid.step(error,dt)
 
         # reference for yaw control
         # Ackermann Steering dynamics http://correll.cs.colorado.edu/?p=1869
@@ -47,5 +38,6 @@ class Controller(object):
             if val*self.decel_limit <self.brake_deadband:
                 # rospy.logdebug("Dead band, %.03f",val*self.decel_limit)
                 return 0.0,0.0,steering
-            return 0.0,val*self.decel_limit,steering
+            return 0.0, val*self.decel_limit,steering
+
 

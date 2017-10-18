f9d5fe3d7ad7279a206d9018746af91554575076
Tianzi Cai <tianzih@outlook.com>
2017-10-18 00:16:51 -0400

    Turn off logwarn for TL state

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -64,7 +64,7 @@ class TLClassifier(object):
 
     @staticmethod
     def class_to_traffic_light(klass):
-        rospy.logwarn("klass :%s", klass)
+        #rospy.logwarn("klass :%s", klass)
         if klass == 1:
             return TrafficLight.RED
         elif klass == 2:

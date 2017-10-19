885ce55af4ee7e9d2ac56314fd735e167799f0ae
Yusun.Liu <liuyusun@hotmail.com>
2017-10-19 06:25:04 -0700

    clean function in tl_classifiier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -52,46 +52,6 @@ class TLClassifier(object):
 
         return TLClassifier.class_to_traffic_light(klass)
 
-    def get_classification1(self, image):
-        """Determines the color of the traffic light in the image
-
-        Args:
-            image (cv::Mat): image containing the traffic light
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        with self.model.as_default():
-            with tf.Session(graph=self.model) as sess:
-                # Definite input and output Tensors for detection_graph
-                image_tensor = self.model.get_tensor_by_name('image_tensor:0')
-                # Each box represents a part of the image where a particular object was detected.
-                detection_boxes = self.model.get_tensor_by_name('detection_boxes:0')
-                # Each score represent how level of confidence for each of the objects.
-                # Score is shown on the result image, together with the class label.
-                detection_scores = self.model.get_tensor_by_name('detection_scores:0')
-                detection_classes = self.model.get_tensor_by_name('detection_classes:0')
-                num_detections = self.model.get_tensor_by_name('num_detections:0')
-
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_np_expanded = np.expand_dims(image, axis=0)
-
-                # Actual detection.
-                (boxes, scores, classes, num) = sess.run(
-                  [detection_boxes, detection_scores, detection_classes, num_detections],
-                  feed_dict={image_tensor: image_np_expanded})
-
-                boxes = np.squeeze(boxes)
-                scores = np.squeeze(scores)
-                classes = np.squeeze(classes).astype(np.int32)
-
-                if len(scores) < 1:
-                    return TrafficLight.UNKNOWN;
-                
-                klass = classes[0]
-                
-                return TLClassifier.class_to_traffic_light(klass)
 
     @staticmethod
     def class_to_traffic_light(klass):
